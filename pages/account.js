961d36861d5883c49554f268d59ef68bdfb677c5
Abhishekkushwaha15 <kushwahaabhi@outlook.in>
2026-08-22 18:54:45 +0530

    updata

--- pages/account.js
+++ pages/account.js
@@ -3,5 +3,3 @@ export function render() { return '<section data-page="account"></section>'; }
 // export function render() { return '<section data-page="about"></section>'; }
 // export function render() { return '<section data-page="about"></section>'; }
 // export function render() { return '<section data-page="about"></section>'; }
-// export function render() { return '<section data-page="about"></section>'; }
-// export function render() { return '<section data-page="about"></section>'; }
